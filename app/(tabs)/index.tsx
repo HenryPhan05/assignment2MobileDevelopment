1f188e70248d325b0616ba18740723bfde8eb38d
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-02-02 16:04:39 -0700

    push the template assignment

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+
+import { StyleSheet, View, Text } from 'react-native';
+
+
+
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={styles.color}>h1</Text>
+    </View >
+  );
+}
+
+const styles = StyleSheet.create({
+  color: {
+    color: "white"
+  }
+});
